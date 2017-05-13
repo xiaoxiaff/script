2a7998a6614e7dfa10775dbda490a203f0daae93
Ariel Xin <yuexin@cs.ucla.edu>
2017-05-13 14:49:58 -0700

    added script for kallisto

---
+++ kallisto_utils.py
@@ -0,0 +1,29 @@
+from general_utils import execute_command
+# https://pachterlab.github.io/kallisto/manual
+
+# k: default and maximum k is 31, must be odd
+def build_index_with_k(transcriptome_reference_file, k, index_output_path):
+	command = "kallisto index -i " \
+		+ index_output_path \
+		+ " -k " \
+		+ str(k) \
+		+ " " \
+		+ transcriptome_reference_file
+	
+	execute_command(command, True)
+
+
+def quant(index_dir, output_dir, sample_pair1, sample_pair2):
+	command = "kallisto quant -i " \
+		+ index_dir \
+		+ " -o " \
+		+ output_dir \
+		+ " " \
+		+ sample_pair1 \
+		+ " " \
+		+ sample_pair2 \
+
+	execute_command(command, True)
+
+# build_index_with_k("chr22_small.fa", 29, "index2")
+# quant("index2", "output2", "simulated_reads/sample_01_1.fasta", "simulated_reads/sample_01_2.fasta")
