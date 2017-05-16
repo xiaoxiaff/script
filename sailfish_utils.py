3fbc7fbb5732f075823b2966248cf23818ce4ae1
lyuanqi <liyuanqi@wifi-131-179-50-217.host.ucla.edu>
2017-05-15 19:24:46 -0700

    add lib_dir for sailfish command

--- sailfish_utils.py
+++ sailfish_utils.py
@@ -1,8 +1,11 @@
 from general_utils import execute_command
 
 
+lib_dir = "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/cs229/sailfish/Sailfish-0.6.3-Linux_x86-64/lib"
+
 def build_index(k, transcriptome_reference_file, index_output_path):
-    command = "sailfish index -t " \
+    command = lib_dir \
+        + "sailfish index -t " \
         + transcriptome_reference_file \
         + " -i " \
         + index_output_path \
@@ -14,7 +17,8 @@ def build_index(k, transcriptome_reference_file, index_output_path):
 
 def quant_with_k(k, sample_pair1,sample_pair2, index_dir, output_dir, transcriptome_reference_file, index_output_path):
     build_index(k, transcriptome_reference_file, index_output_path)
-    command = "sailfish quant -i " \
+    command = lib_dir \
+        + "sailfish quant -i " \
         + index_dir \
         + " -l " \
         + "T=PE:O=><:S=SA" \
