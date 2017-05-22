07b051797a5ff49587149e8b1ede1eb37f58b1d9
Yuanqi Li <lyuanqi@ucla.edu>
2017-05-22 04:05:39 -0700

    add outer loop for differet simulation settings

--- quantification_script.py
+++ quantification_script.py
@@ -1,3 +1,4 @@
+import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -9,50 +10,62 @@ import sailfish_utils as sailfish
 import kallisto_utils as kallisto
 
 
-OS = "ubuntu"
+OS = sys.platform
 
-if(OS=="ubuntu"):
-    project_dir = "/home/ubuntu/cs229"
-    simulation_script_path = project_dir + "/CS229S_Project/simulation_script.R"
-else:
+print("Running on platform: " + OS)
+print("Setting global variables...")
+
+# mac
+if(OS=="darwin"):
     project_dir = "/Users/liyuanqi/Google_Drive/UCLA_MSCS/Quarter3/CS229S/Project"
     simulation_script_path = project_dir + "/simulation_script.R"
+#ubuntu
+else:
+    project_dir = "/home/ubuntu/cs229"
+    simulation_script_path = project_dir + "/CS229S_Project/simulation_script.R"
 
-####### Universal Settings ###########################
 transcriptome_reference_file = project_dir + "/chr22_small.fa"
 simulated_reads_dir = project_dir + "/simulated_reads"
 
 
-####### Salmon Settings #############################
-salmon_index_dir = project_dir + "/salmon/index"
-salmon_output_dir = project_dir + "/salmon/output"
+################### Settings ############################
+
+k_range = np.arange(11,14,2)
+coverage_range = np.arange(20,50,10)
+error_rate_range = np.arange(0.0,0.1,0.005)
+readlen_range = np.arange(0.0,0.1,0.005)
+
+number_of_transcripts = 10
+default_readlen = 100
+default_error_rate = 0.005
+default_coverage = 20
+
+##########################################################
 
 
-####### Sailfish Settings ###########################
-sailfish_index_dir = project_dir + "/sailfish/index"
-sailfish_output_dir = project_dir + "/sailfish/output"
+def get_index_dir_by_toolname(tool_name):
+    return project_dir + "/" + tool_name + "/index"
 
 
-####### Kallisto Settings ###########################
-kallisto_index_dir = project_dir + "/kallisto/index"
-kallisto_output_dir = project_dir + "/kallisto/output"
-
+def get_output_dir_by_toolname(tool_name):
+    return project_dir + "/" + tool_name + "/output"
 
 
 def run_with_k(k, ground_truth_map, transcriptome_reference_file, simulated_reads_dir):
     print("quant with k=" + str(k) + "...")
     # salmon
-    salmon_quantificatoin_map = salmon.run_salmon(k, transcriptome_reference_file, salmon_index_dir, simulated_reads_dir, salmon_output_dir)
+    salmon_quantificatoin_map = salmon.run_salmon(k, transcriptome_reference_file, get_index_dir_by_toolname("salmon"), simulated_reads_dir, get_output_dir_by_toolname("salmon"))
     salmon_error = get_average_percentage_error(ground_truth_map, salmon_quantificatoin_map)
     
     # kallisto, max allowed k=31
-    kallisto_quantificatoin_map = kallisto.run_kallisto(k, transcriptome_reference_file, kallisto_index_dir, simulated_reads_dir, kallisto_output_dir)
+    kallisto_quantificatoin_map = kallisto.run_kallisto(k, transcriptome_reference_file, get_index_dir_by_toolname("kallisto"), simulated_reads_dir, get_output_dir_by_toolname("kallisto"))
     kallisto_error = get_average_percentage_error(ground_truth_map, kallisto_quantificatoin_map)
     
     return salmon_error, kallisto_error
 
 
-def plot_result(k_range, salmon_errors, kallisto_errors):
+def plot_result(plot_name, k_range, salmon_errors, kallisto_errors):
+    plt.figure()
     n_groups = len(k_range)
     index = np.arange(n_groups)
 
@@ -71,40 +84,41 @@ def plot_result(k_range, salmon_errors, kallisto_errors):
     plt.xticks(index + bar_width/2.0, k_range)
     plt.legend()
     plt.tight_layout()
-    plt.savefig(project_dir + "/k")
+    print(project_dir + "/" + plot_name)
+    plt.savefig(project_dir + "/" + plot_name)
 
 
-def main():
-    number_of_transcripts = 10
-    readlen = 100
-    error_rate = 0.001
-    coverage = 20
-    output_dir = project_dir
+def run_with_simulation_parameters(number_of_transcripts, readlen, error_rate, coverage):
+    print("Simulation settings:")
+    print("\treadlen = " + str(readlen))
+    print("\terror_rate = " + str(error_rate))
+    print("\tcoverage = " + str(coverage))
 
-    ground_truth_map = simulate_reads(
-        simulation_script_path,
-        number_of_transcripts,
-        readlen,
-        error_rate,
-        coverage,
-        output_dir)
-
-    # sailfish.quant_with_k(31, 
-    #     simulated_reads_dir + "/sample_01_1.fasta",
-    #     simulated_reads_dir + "/sample_01_2.fasta",
-    #     sailfish_index_dir,
-    #     sailfish_output_dir,
-    #     transcriptome_reference_file)
-
-    k_range = range(11,14,2)
+    ground_truth_map = simulate_reads(simulation_script_path, number_of_transcripts, readlen, error_rate, coverage, project_dir)
     salmon_errors = []
     kallisto_errors = []
     for k in k_range:
         salmon_error, kallisto_error = run_with_k(k, ground_truth_map, transcriptome_reference_file, simulated_reads_dir)
         salmon_errors.append(salmon_error)
         kallisto_errors.append(kallisto_error)
+    plot_name = "readlen"+str(readlen)+"_error"+str(error_rate).replace(".","-")+"_coverage"+str(coverage)
+    
+    plot_result(plot_name, k_range, salmon_errors, kallisto_errors)
 
-    plot_result(k_range, salmon_errors, kallisto_errors)
+
+def main():
+    # loop coverage
+    for coverage in coverage_range:
+        run_with_simulation_parameters(number_of_transcripts, default_readlen, default_error_rate, coverage)
+
+    # # loop error_rate
+    # for error_rate in error_rate_range:
+    #     run_with_simulation_parameters(number_of_transcripts, default_readlen, error_rate, default_coverage)
+
+    # # loop read_len
+    # for readlen in readlen_range:
+    #     run_with_simulation_parameters(number_of_transcripts, readlen, default_coverage, default_coverage)
+
 
 if __name__ == "__main__":
     main()
